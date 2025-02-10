2117ec285370d89dd6f53ba4a19d6121b4201fd3
Steven-Roduta <stevenroduta00@gmail.com>
2025-02-10 15:10:35 +0100

    feat: add image and style in css

--- react-router/src/layouts/DefaultLayout.jsx
+++ react-router/src/layouts/DefaultLayout.jsx
@@ -4,7 +4,17 @@ export default function DefaultLayout() {
     return (
         <>
         <Header/>
-        <h1>Menu</h1>
+        
+        <div className="container"></div>
+
+        <div className="description">
+            <h1>138 Storie</h1>
+            <p>Pasticceria 138 Canti è un luogo dove la tradizione dolciaria si unisce alla passione per l'arte del gusto. Con una selezione di dolci freschi e preparati con ingredienti di alta qualità, ogni prodotto è una vera e propria melodia per il palato. Dalle torte classiche alle creazioni innovative, ogni morso racconta una storia di dolcezza e perfezione. Vieni a scoprire la magia della Pasticceria 138 Canti, dove ogni momento è un'esperienza unica.</p>
+        </div>
+        <div className="bakery">
+         <img src="/img/pasticceria.webp" alt="Pasticceria 138 Canti"/>
+        </div>
+
         </>
     )
 }
